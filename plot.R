3e38a9acb07d413ee1ca751fa5e67bf905fd4fe3
Dmitriy Khodakov <dmitryhd@gmail.com>
2014-10-07 13:43:14 +0400

    plot fix

--- plot.R
+++ plot.R
@@ -3,6 +3,8 @@
 # Author: Dmitriy Khodakov <dmitryhd@gmail.com>
 # Date: 29.09.2014
 
+header <- 'Зарплата программистов (hh.ru 29.09.2014, Khodakov)'
+
 # 1. load dataset.
 pvac <- read.csv("data/pvac.csv", sep=";", stringsAsFactors=FALSE)
 
@@ -20,6 +22,7 @@ datasets <- list(pvac[which(pvac$python==1),],
               )
 
 descriptions <- c('python','cpp','java','php','perl','ruby','bash','javascript','x1c','sap')
+readable_descriptions <- c('Python','C++','Java','php','Perl','Ruby','Bash','JavaScript','1C','SAP')
 
 # 3. Function for plotting boxplots.
 getdf <- function(datasets, descriptions, file_name) {
@@ -35,42 +38,40 @@ getdf <- function(datasets, descriptions, file_name) {
   mns <- c() # means
   s_max <- list()
   s_min <- list()
+  min_means <- c()
+  max_means <- c()
   for (i in 1:length(datasets)) {
     data <- datasets[[i]]
-    print(descriptions[i])
-    pm1 <- as.numeric(data$max_salary)
-    need_size <- max_size - length(data$max_salary)
-    new_max <- c(pm1, rep(NA, need_size))
-    pm2 <- as.numeric(data$min_salary)
-    need_size <- max_size - length(data$min_salary)
-    new_min <- c(pm2, rep(NA, need_size))
-    mn <- mean(c(pm1, pm2), na.rm=T)
-    max_name <- paste(descriptions[i],'_max',sep = "")
-    min_name <- paste(descriptions[i],'_min',sep = "")
-    mns <- c(mns, mn)
-    names_min <- c(names_min, min_name)
-    names_max <- c(names_max, max_name)
-    names <- c(names, descriptions[i])
-    s_max[[i]] <- new_max
-    s_min[[i]] <- new_min
+    names <- c(names, readable_descriptions[i])
+
+    max_slary <- as.numeric(data$max_salary)
+    s_max[[i]] <- c(max_slary, rep(NA, max_size - length(data$max_salary)))
+    names_max <- c(names_max, paste(descriptions[i],'_max',sep = ""))
+    max_means <- c(max_means, mean(max_slary, na.rm=T))
+
+    min_slary <- as.numeric(data$min_salary)
+    s_min[[i]] <- c(min_slary, rep(NA, max_size - length(data$min_salary)))
+    names_min <- c(names_min, paste(descriptions[i],'_min',sep = ""))
+    min_means <- c(min_means, mean(min_slary, na.rm=T))
+
+    mns <- c(mns, mean(c(max_slary, min_slary), na.rm=T))
   }  
   # 3.3. store data in dataframe df, in sorted order by mean 
   df <- data.frame(rep(NA, max_size))
-  prepared_data <- data.frame(mns, names_min, names_max, names, stringsAsFactors=FALSE)
+  prepared_data <- data.frame(mns, names_min, names_max, names, min_means, max_means, stringsAsFactors=FALSE)
   plot_labels <- c()
   for (i in with(prepared_data, order(mns))) {
-    print(prepared_data[i,2])
     df[prepared_data[i,3]] <- s_max[[i]]
     df[prepared_data[i,2]] <- s_min[[i]]
-    plot_labels <- c(plot_labels, paste(prepared_data[i,4], ' мин. [Сердн.=', format(round(prepared_data[i,1], -3), scientific=F), '] ', 'Вакансий:', length(datasets[[i]]$max_salary), sep=''))
-    plot_labels <- c(plot_labels, paste(prepared_data[i,4], ' мaкс. [Сердн.=', format(round(prepared_data[i,1], -3), scientific=F), '] ', 'Вакансий:', length(datasets[[i]]$max_salary), sep=''))
+    plot_labels <- c(plot_labels, paste(prepared_data[i,4], ' "До". [Сердн.=', format(round(prepared_data[i,6], -2), scientific=F), '] ', 'Вакансий:', length(datasets[[i]]$max_salary), sep=''))
+    plot_labels <- c(plot_labels, paste(prepared_data[i,4], ' "От". [Сердн.=', format(round(prepared_data[i,5], -2), scientific=F), '] ', 'Вакансий:', length(datasets[[i]]$max_salary), sep=''))
   }
   df$rep.NA..max_size. <- NULL
 
   # 3.4 plot
   png(file_name, width = 1200, height = 800)
   par(mar=c(4, 20, 2, 0.5))
-  boxplot(df, xaxt='n', las = 2, ylim=c(20000, 200000), names=plot_labels, horizontal=T, col=rainbow(length(df)), main='Зарплата программистов (hh.ru 29.09.2014, Khodakov)', xlab='Руб.')
+  boxplot(df, xaxt='n', las = 2, ylim=c(20000, 200000), names=plot_labels, horizontal=T, col=rainbow(length(df)), main=header, xlab='Руб.')
   abline(v = seq(10000, 300000, 10000), lty=1.5, lwd=0.5, col=336)
   axis(side = 1, at = seq(10000, 300000, 10000))
   dev.off()
