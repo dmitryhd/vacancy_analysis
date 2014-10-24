d1858307724d410ed57f426fdb82f0813848c4a9
Dmitry Khodakov <dmitryhd@gmail.com>
2014-10-24 05:53:15 +0400

    #3 additional sites - begin

--- config.py
+++ config.py
@@ -26,6 +26,8 @@ tag_name = 0
 tag_text = 1
 tag_title = 2
 
+Urls = {'hh.ru': BASE_URL,
+        'sj.ru': 'http://www.superjob.ru/vacancy/search/?sbmit=1&t[]=4&keywords[0][srws]=10&keywords[0][skwc]=and&keywords[0][keys]=%D0%BF%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%B8%D1%81%D1%82&search_hesh=961543413088896&main=1'}
 BASE_URL = 'http://hh.ru/search/vacancy?source=&text=%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%81%D1%82&specialization=1&specialization=8&specialization=12&specialization=14&area=1&salary=&currency_code=RUR&only_with_salary=true&experience=&employment=full&order_by=relevance&search_period=&items_on_page=100&no_magic=true&page=1'
 TEST_BASE_URL = 'http://hh.ru/search/vacancy?source=&text=%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%B8%D1%81%D1%82&specialization=1&specialization=8&specialization=12&specialization=14&area=1&salary=&currency_code=RUR&only_with_salary=true&experience=&employment=full&order_by=relevance&search_period=&items_on_page=10&no_magic=true&page=1'
 MAXIM_NUMBER_OF_PAGES = 1000
--- site_parser.py
+++ site_parser.py
@@ -100,4 +100,58 @@ class SiteParserHH(SiteParser):
         return vacancies
 
 
+class SiteParserSJ(SiteParser):
+    """ Implementation of Head hunter parser. """
+
+    def get_vacancy(self, name, html, url):
+        """ Get base vacancy by name and html code of page. """
+        new_html = ''
+        soup = SiteParser._sanitize_html(html)
+        res = soup.find('div', class_='b-important b-vacancy-info')
+        if res:
+            new_html += res.decode()
+        res = soup.find('table',
+                        class_='l-content-2colums b-vacancy-container')
+        if res:
+            new_html += res.text
+        return Vacancy(name, new_html, url=url, site='hh.ru')
+
+    def get_vacancies_on_page(self, url, vacancies, session, maximum_vac):
+        """ Download all vacancies from page and return link to next page. """
+        page = self.get_url(url)
+        soup = bs4.BeautifulSoup(page)
+        print('')
+        for vacancy in soup.find_all('div', class_='searchresult__name'):
+            name = vacancy.string
+            if name is not None:
+                link = vacancy.find_all('a')[0].attrs["href"]
+                vacancy_html = self.get_url(link)
+                new_vacancy = self.get_vacancy(name, vacancy_html, link)
+                vacancies.append(new_vacancy)
+                session.add(new_vacancy)
+                session.commit()
+                if len(vacancies) >= maximum_vac:
+                    return None
+                stdout.write("\rdownloaded {} vacancy".format(new_vacancy.id))
+                stdout.flush()
+        try:
+            link = soup.find_all('a',
+                                 class_='b-pager__next-text')[1].attrs["href"]
+        except IndexError:
+            return None
+        next_link = 'http://hh.ru' + link
+        return next_link
+
+    def get_all_vacancies(self, session,
+                          maximum_vac=MAXIM_NUMBER_OF_VACANCIES):
+        """ Must return list of Vacancies. """
+        vacancies = []
+        next_link = self.base_url
+        for i in range(maximum_vac):
+            next_link = self.get_vacancies_on_page(next_link, vacancies,
+                                                   session, maximum_vac)
+            if next_link == None:
+                break
+        return vacancies
+
 ParserImpl = {'hh.ru': SiteParserHH}
