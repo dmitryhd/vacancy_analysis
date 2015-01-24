e3e60e5d55c5e0430a168159bca486cb9f4097fd
Dmitriy Khodakov <dmitryhd@gmail.com>
2015-01-24 20:30:34 +0400

    Seems fine

---
+++ README.rst
@@ -0,0 +1,12 @@
+Vacancy analysis
+================
+Web crawler and data analizer with web interface. 
+Currently used to gather information about programmer vacancies.
+
+How to use it:
+
+1. Create data folders: ** mkdir -p /opt/vacan/data; mkdir -p /opt/vacan/common; **
+2. Download statistics info to vacancy database: ** ./vacan_proc -c **
+3. Run web interface: ** ./vacan_web **
+4. Open http://localhost:9999 in your favorite browser and see results.
+
--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from setuptools import setup, find_packages  # Always prefer setuptools over distutils
 from codecs import open  # To use a consistent encoding
 from os import path
@@ -5,50 +7,32 @@ from os import path
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
-with open(path.join(here, 'DESCRIPTION.rst'), encoding='utf-8') as f:
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
     name = 'vacancy_analysis',
-
-    # Versions should comply with PEP440.  For a discussion on single-sourcing
-    # the version across setup.py and the project code, see
-    # https://packaging.python.org/en/latest/single_source_version.html
-    version = '1.1.0',
-
+    version = '0.3.0',
     description = 'Web crawler and data analizer with web interface.'
                   'Currently used to gather information about programmer '
                   'vacancies.',
     long_description=long_description,
-
-    # The project's main homepage.
     url = 'https://github.com/dmitryhd/vacancy_analysis',
-
     # Author details
     author='Dmitriy Khodakov',
     author_email = 'dmitryhd@gmail.com',
-
     # Choose your license
     license = 'MIT',
-
     # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        # How mature is this project? Common values are
-        #   3 - Alpha
-        #   4 - Beta
-        #   5 - Production/Stable
         'Development Status :: 4 - Beta',
-
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
         'Topic :: Internet :: WWW/HTTP :: Indexing/Search',
         'Topic :: Scientific/Engineering :: Information Analysis',
         'Topic :: Scientific/Engineering :: Visualization',
-
-
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: Apache Software License',
-
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
@@ -59,12 +43,11 @@ setup(
 
     # What does your project relate to?
     keywords='crawler data analysis text processing',
-
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
-
+    packages=find_packages(exclude=['contrib', 'docs']),
     # List run-time dependencies here.  These will be installed by pip when
+
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
@@ -79,17 +62,6 @@ setup(
         'test': ['coverage'],
     },
     include_package_data = True,
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages.
-    # see 
-    # http://docs.python.org/3.4/distutils/setupscript.html
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    #data_files=[('my_data', ['data/data_file'])],
 
     # To provide executable scripts, use entry points in preference to the
     # "scripts" keyword. Entry points provide cross-platform support and allow
