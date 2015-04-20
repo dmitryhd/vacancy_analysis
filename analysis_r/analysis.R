9a6c42b51139abceead15792baa63e085fc459ae
Dmitriy Khodakov <dmitryhd@gmail.com>
2015-04-20 22:55:16 +0300

    all basics done

--- analysis_r/analysis.R
+++ analysis_r/analysis.R
@@ -16,20 +16,17 @@ load_dataset <- function() {
   vacan
 }
 
-category_to_salary <- function(category, bin.size) {
-  category * bin.size
-}
-
-make_categories <- function(vacan) {
+make_categories <- function(vacan, bin.number) {
   # fill NAs in salary: if min NA, set to max, if max is NA, set to min
   vacan$min_sal[is.na(vacan$min_sal)] <- vacan$max_sal[is.na(vacan$min_sal)]
   vacan$max_sal[is.na(vacan$max_sal)] <- vacan$min_sal[is.na(vacan$max_sal)]
   vacan$mean_sal <- vacan$min_sal + (vacan$max_sal - vacan$min_sal)/2
   norm_sal <- remove_outliers(vacan$mean_sal)
-  bin.number <- 10
   bin.size <- (max(norm_sal, na.rm=TRUE) - min(norm_sal, na.rm=TRUE) ) / bin.number
+  print(bin.size)
   vacan$category <- as.integer(norm_sal / bin.size)
   #cl <- factor(vacan.category)
+  #vacan[is.na(vacan$category)]$category <- 0
   vacan
 }
 
@@ -41,14 +38,13 @@ shuffle_data <- function(vacan, seed=1) {
 }
 
 get_train <- function(vacan) {
-  upper <- dim(vacan)[1]*.9
+  upper <- dim(vacan)[1]*.8
   train <- vacan[1:upper,]
-  train[is.na(train$category)]$category <- 11
   train
 }
 
 get_test <- function(vacan) {
-  lower <- dim(vacan)[1]*.9
+  lower <- dim(vacan)[1]*.8
   upper <- dim(vacan)[1]
   test <- vacan[lower:upper,]
   test
@@ -69,35 +65,40 @@ strip_set <-function(set) {
   stripped
 }
 
-# data preparation
-
-vacan <- load_dataset()
-vacan <- make_categories(vacan)
-train_and_test <- create_training_sample(vacan, 1)
-train <- train_and_test[[1]]
-test <- train_and_test[[2]]
-
-cl <- factor(train$category)
-strip_train <- strip_set(train)
-strip_test <- strip_set(test)
-valid <- test$category
-
-# classification
-
 library(class)
-cl[is.na(cl)] <- 0 # TODO - move
-result <- knn(strip_train, strip_test, cl, k = 10) 
-summary(result)
-calculate_precision(test$category, as.integer(result))
 
-errors <- abs(test$category - as.integer(result))
-errors_stat <- ecdf(errors)
-plot(errors_stat)
-
-
-
-calculate_precision <- function(expected, result) {
-  sum(expected - result == 0)/length(expected)
+estimate_classification <- function(seed=1, given.k=1, bin.number=10) {
+  # data preparation
+  vacan <- load_dataset()
+  vacan <- make_categories(vacan, bin.number)
+  train_and_test <- create_training_sample(vacan, seed)
+  train <- train_and_test[[1]]
+  test <- train_and_test[[2]]
+  
+  # classification
+  cl <- factor(train$category)
+  strip_train <- strip_set(train)
+  strip_test <- strip_set(test)
+  valid <- test$category
+  
+  # estimatin
+  cl[is.na(cl)] <- 0 # TODO - move
+  result <- knn(strip_train, strip_test, cl, k=given.k) 
+  errors <- abs(test$category - as.integer(result))
+  errors
 }
 
+print_classification_error <- function(bin.number=10, k=1) {
+  err <- estimate_classification(seed=1, given.k=k, bin.number)
+  err_stat <- ecdf(err)
+  plot(err_stat, main = paste0('Error, k=', k, ', bin=', bin.number))
+}
 
+print_classification_error(10, 1)
+print_classification_error(15, 1)
+print_classification_error(15, 2)
+print_classification_error(15, 4)
+print_classification_error(15, 6)
+print_classification_error(15, 8)
+print_classification_error(15, 10)
+print_classification_error(15, 20)
